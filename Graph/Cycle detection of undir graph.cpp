7bc9234adbcbdd7a2d74a7a8f7864fcd94f2f9ac
Ankit Tyagi <88319247+ancorp134@users.noreply.github.com>
2022-07-05 22:16:18 +0530

    new file

--- Graph/Cycle detection of undir graph.cpp
+++ Graph/Cycle detection of undir graph.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
+bool cycleDFS(int node,int par,vector<bool>& vis ,vector<vector<int>>& adj){
+        vis[node]=true;
+        
+        for(auto i : adj[node]){
+            if(!vis[i]){
+                if(cycleDFS(i,node,vis,adj)) return true;
+            }
+            else if(vis[i] && par!=i) return true; 
+        }
+        return false;
+    }
+
+// BFS Approach
+bool cycleBFS(int i,vector<bool>& vis ,vector<vector<int>>& adj ){
+        queue<pair<int,int>> q;
+        q.push(make_pair(i,-1));
+        vis[i]=true;
+        while(!q.empty()){
+            int node=q.front().first;
+            int par=q.front().second;
+            q.pop();
+            for(auto v:adj[node]){
+                if(!vis[v]){
+                    q.push(make_pair(v,node));
+                    vis[v]=true;
+                }
+                else if(vis[v] && par!=v) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 int main(){
     int n,m;
     cin>>n>>m;
@@ -13,5 +49,18 @@ int main(){
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+    
+    vector<bool> vis(n+1,false);
+
+    int flag=0;
+
+    for(int i=0;i<=n;i++){
+        if(!vis[i]){
+            if(cycleDFS(i,-1,vis,adj)) flag=1;
+        }
+    }
+
+    if(flag) cout<<"YES";
+    else cout<<"NO";
     
 } 
